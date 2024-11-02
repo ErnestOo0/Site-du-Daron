016a603428e80bcac061095c4ab7b9c339846be8
ErnestOo0 <139120658+ErnestOo0@users.noreply.github.com>
2024-11-02 11:53:45 +0100

    les cards sont devenues des box, elles grossissent et ont une ombre lorsqu'on les survolle

--- aff.js
+++ aff.js
@@ -8,14 +8,32 @@ function goToLinkNewTab(link){
     window.open(link,"_blank");
 }
 
+function survolIn(idCard){
+    console.log("in "+ idCard);
+    //$("#"+idCard).color = "red";
+    document.getElementById(idCard).style.boxShadow = "10px 5px 5px rgb(82, 82, 82)";
+    document.getElementById(idCard).style.zoom = "1.1";
+}
+
+function survolOut(idCard){
+    console.log("out "+idCard);
+    //$("#"+idCard).color = "black";
+    document.getElementById(idCard).style.boxShadow = "0px 0px 0px white";
+    document.getElementById(idCard).style.zoom = "1";
+}
+
 function getData(data){
     $.getJSON("http://localhost:8080/data/"+data+".json",function(fileData){
         console.log(fileData);
+        let num = 0;
+        let nomId = "art"
         for(let a of fileData){
             console.log(a["nom"]);
-            let link = "https://www.ixocollections.com/img/cms/Porsche%20edito%2026/PORSCHE-Carrera-GT--DP-3-.jpg";
+            console.log(nomId+num);
 
-            $("#listeArt").append("<div class='col-sm-6 col-md-4 col-lg-3 px-1 py-1'><div class='card' onclick='goToLinkNewTab(\""+a["link"]+"\")'><div class='bg' style='background-image:linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)),url(\""+a["image"]+"\")'><h3>"+a["nom"]+"</h3><h4>"+a["revue"]+"</div></div></div>");
+            $("#listeArt").append("<div class='col-sm-6 col-md-4 col-lg-3 px-1 py-1'><div class='box' id='"+nomId+num+"' onmouseover='survolIn(\""+nomId+num+"\")' onmouseout='survolOut(\""+nomId+num+"\")' onclick='goToLinkNewTab(\""+a["link"]+"\")' style='background-image:linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)),url(\""+a["image"]+"\")'><h3>"+a["nom"]+"</h3><h4>"+a["revue"]+"</div></div>");
+
+            num += 1;
         }
     });
 }
--- serveur.js
+++ serveur.js
@@ -6,17 +6,22 @@ app.listen(PORT);
 console.log("demarage du serveur sur le port "+PORT);
 
 app.get('/', (request, response) => {
-    response.sendFile('homePage.html', {root: __dirname});
+    response.sendFile('home/homePage.html', {root: __dirname});
     console.log("nouvelle connexion")
 });
 
-app.get('/file/:file', (request, response) => {
+app.get('/js/:file', (request, response) => {
     response.sendFile(request.params.file, {root: __dirname});
+    console.log("envoie du fichier js ", request.params.file);
+});
+
+app.get('/file/:folder/:file', (request, response) => {
+    response.sendFile(request.params.folder+"/"+request.params.file, {root: __dirname});
     console.log("envoie de ", request.params.file);
 });
 
 app.get('/data/:data', (request, response) => {
-    response.sendFile(request.params.data, {root: __dirname});
+    response.sendFile("data/"+request.params.data, {root: __dirname});
     //response.json(request.params.data);
     console.log("envoie du fichier ", request.params.data);
 });
